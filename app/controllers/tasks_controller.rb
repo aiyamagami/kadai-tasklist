e902755cc2b8a1ffa114b71abc2d933f2cbfba38
ai.yamagami <ai.yamagami@gmail.com>
2017-05-20 17:27:49 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,56 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    set_task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(message_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    set_task
+  end
+
+  def update
+    set_task
+    if @task.update(message_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    set_task
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
   
-  
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def message_params
+    params.require(:task).permit(:content, :status)
+  end
 end
